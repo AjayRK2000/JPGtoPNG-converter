59625e6727af669e5c1d14e9aee5c17a89595b49
AjayRK2000 <108030094+AjayRK2000@users.noreply.github.com>
2022-07-11 19:03:56 -0400

    improve variable names

--- JPG_to_PNG.py
+++ JPG_to_PNG.py
@@ -7,32 +7,33 @@ if(len(sys.argv) < 3):
     quit()
 
 # grab the first and second arguments from the Terminal, ensure they aren't blank
-from_file = sys.argv[1]
-to_file = sys.argv[2]
+jpg_img_folder = sys.argv[1]
+png_img_folder = sys.argv[2]
 
-if len(from_file) < 1 or len(to_file) < 1:
+if len(jpg_img_folder) < 1 or len(png_img_folder) < 1:
     print("Please try again and enter 2 file paths.")
     quit()
 
 
 # ensure that Terminal arguments are in fact directories
-if os.path.isdir(from_file):
-    if not from_file.endswith('/'):
-        from_file += '/'
+if os.path.isdir(jpg_img_folder):
+    if not jpg_img_folder.endswith('/'):
+        jpg_img_folder += '/'
 
-if not to_file.endswith('/'):
-    to_file += '/'
+if not png_img_folder.endswith('/'):
+    png_img_folder += '/'
 
-print(f'Images in {from_file} will be converted and saved to {to_file}')
+print(
+    f'Images in {jpg_img_folder} will be converted and saved to {png_img_folder}')
 
 # check if new folder exists, if not create it
-if not (os.path.isfile(to_file) or os.path.exists(to_file)):
-    os.mkdir(to_file)
+if not (os.path.isfile(png_img_folder) or os.path.exists(png_img_folder)):
+    os.mkdir(png_img_folder)
 
 # loop through the entire folder, convert each JPG to PNG and save it in new folder
-for filename in os.listdir(from_file):
+for jpg_file in os.listdir(jpg_img_folder):
     # Remove extension from name of original file
-    clean_name = os.path.splitext(filename)[0]
-    with Image.open(f'{from_file}{filename}') as img:
-        img.save(f"{to_file}{clean_name}.png", "png")
-        print(f'{clean_name}.jpg converted.')
+    filename_without_extension = os.path.splitext(jpg_file)[0]
+    with Image.open(f'{jpg_img_folder}{jpg_file}') as img:
+        img.save(f"{png_img_folder}{filename_without_extension}.png", "png")
+        print(f'{filename_without_extension}.jpg converted.')
